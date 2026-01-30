c38869c62b2b0593b0a40acd099e5af916f50f78
kesche07 <keschellap@gmail.com>
2026-01-30 13:57:13 +0545

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,22 @@
+const params = new URLSearchParams(window.location.search); //gets query parameter from url 
+const index = params.get("id"); // gets index from url
 
+//if id exists, product is displayed
+if (index != null) {
+    showProduct(index);
+}
+
+function showProduct(index) {
+    const products = document.getElementsByClassName("product"); //gets elements with class name product
+
+    for (var i = 0; i < products.length; i++) //loops through each product
+        { 
+        // Hide everything
+        products[i].style.display = "none";
+
+        // Show the matching product
+        if (products[i].getAttribute("data-index") == index) {
+            products[i].style.display = "flex";
+        }
+    }
+}
